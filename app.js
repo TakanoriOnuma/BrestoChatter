20dc6352a680048132009b57d8b55156d2473c64
takanorionuma <s1000607@sendai-nct.jp>
2016-07-27 13:06:05 +0000

    ログイン画面の作成
    ユーザ認証を行い、ログイン前ならログイン画面に遷移するようにした。

--- app.js
+++ app.js
@@ -6,13 +6,37 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// セッションの準備
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+  secret: 'secret',
+  store: new MongoStore({
+    db: 'session',
+    url: process.env.MONGODB_URI || 'mongodb://localhost:27017/chat'
+  }),
+  cookie: {
+    httpOnly: false
+  }
+}));
+
+// 認証用バリデーター関数を用意
+var loginCheck = function(req, res, next) {
+  if(req.session.user) {
+    next();
+  }
+  else {
+    res.redirect('/login');
+  }
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +46,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// ルーティングの設定
+app.use('/login', login);
+app.get('/', loginCheck, routes);
 
 // mongooseを使用してDB設計・操作
 var mongoose = require('mongoose');
@@ -46,7 +71,6 @@ app.get('/chats', function(req, res) {
       res.send(chats);
     });
 });
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
