b19b1cecaa336ea00ef773adc44529e1d38964b2
takanorionuma <s1000607@sendai-nct.jp>
2016-08-08 05:50:27 +0000

    部屋の登録フォームの作成
    ユーザ登録を参考に似たフォームを作成した。

--- routes/room.js
+++ routes/room.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/list', function(req, res, next) {
   res.send([]);
 });
+// 部屋作成画面
+router.get('/registration', function(req, res, next) {
+  res.render('room/registration', { title: '部屋の作成' })
+});
 
 module.exports = router;
