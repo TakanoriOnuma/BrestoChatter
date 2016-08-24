d208161ca937fb6c68d5fbc6b7505e705afb4697
takanorionuma <s1000607@sendai-nct.jp>
2016-08-24 03:24:46 +0000

    フラグの管理
    選択後もう一度クリックすると編集モードに入り、
    編集中はドラッグ移動は出来なくなり、
    ドラッグ移動した後は編集モードには入らない。
    こうした処理を行えるようにフラグを導入し、実現させた。

--- public/javascripts/room/chat/chat.js
+++ public/javascripts/room/chat/chat.js
@@ -207,11 +207,15 @@ angular.module('myApp', ['ui.bootstrap', 'ngSanitize'])
 
         // 元々のタグを保持しておく
         var $span = $('span', element);
-        // クリック時の処理（一度ドラッグするとマウスを上げてもクリック扱いにはならない）
+        var isEditing = false;
+        var isEditable = false;
+        // クリック時の処理
         element.click(function(event) {
           event.stopPropagation();
-          // 既に選択されている時、内容を変更できるようにする
-          if(scope.postIt.selected) {
+          // 2回目のクリック時、内容を変更できるようにする
+          // （mousedownで選択後clickですぐイベントが起きてしまうため）
+          if(isEditable) {
+            isEditing = true;
             // spanタグをtextareaタグに置き換える
             var elem = $('span', element);
             var $input = $('<textarea>').val(scope.postIt.message);
@@ -224,6 +228,7 @@ angular.module('myApp', ['ui.bootstrap', 'ngSanitize'])
 
             // フォーカスが外れたときはモデルに入力内容を反映させて元に戻す
             $input.blur(function(event) {
+              isEditing = false;
               scope.$apply(function() {
                 scope.postIt.message = $input.val();
               });
@@ -232,8 +237,10 @@ angular.module('myApp', ['ui.bootstrap', 'ngSanitize'])
               scope.WebSocket.emit('post-it-contents-change', scope.postIt._id, scope.postIt.message);
             });
           }
+          else {
+            isEditable = true;
+          }
         });
-
 
         // ホワイトボードの基準座標を取得する
         var rootPos   = element.parent().position();
@@ -245,7 +252,12 @@ angular.module('myApp', ['ui.bootstrap', 'ngSanitize'])
         element.mousedown(function(event) {
           pos = element.position();
           offset = { x: event.offsetX, y: event.offsetY };
-          console.log(pos);
+          // 既に選択されていたらこれ以上処理をしない
+          if(scope.postIt.selected) {
+            return;
+          }
+
+          isEditable = false;
           scope.$apply(function() {
             // Ctrlキーが入力されていなければ親スコープを借りて選択状態を全てリセットする
             if(!event.ctrlKey) scope.$parent.reset();
@@ -253,7 +265,8 @@ angular.module('myApp', ['ui.bootstrap', 'ngSanitize'])
           });
         });
         element.parent().mousemove(function(event) {
-          if(offset) {
+          if(offset && !isEditing) {
+            isEditable = false;
             var movedPos = {
               x : event.pageX - offset.x - rootPos.left,
               y : event.pageY - offset.y - rootPos.top
